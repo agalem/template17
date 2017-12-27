7799760165cba957a524e564e57094f4cbf19493
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2017-12-27 22:44:40 +0100

    Google map with API key

--- app/js/map.js
+++ app/js/map.js
@@ -1,6 +1,3 @@
-const slides = document.getElementsByClassName('slide');
-let current  = 0;
-
 function initMap() {
     var newham = {lat: 51.523006, lng: 0.030984};
     var map = new google.maps.Map(document.getElementById('map'), {
